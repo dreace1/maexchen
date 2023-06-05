b52fa8b80e5572dfdb4bb6402dfe941ee31adcf7
Hannes <hannesro27@gmail.com>
2023-06-05 12:56:01 +0200

    created animation for rolling dices

--- dice.py
+++ dice.py
@@ -1,8 +1,8 @@
 import random
+import time
 import pygame
 import const
 
-screen = pygame.display.set_mode((const.GAME_WIDTH, const.GAME_HEIGHT))
 
 class Dice:
     def __init__(self) -> None:
@@ -31,7 +31,7 @@ class Dice:
                 pygame.image.load('assets/roll7.png'),
                 pygame.image.load('assets/roll8.png')]
         
-    def roll(self, dice_roll_counter):
+    def roll(self, screen):
         self.is_rolling = True
         rolling_images_counter = 0
 
@@ -48,11 +48,54 @@ class Dice:
         else:
             self.value = random.randint(1, 6)
 
+    def roll2(self, screen):
+        # self.is_rolling = True
+        # screen.blit(self.dice_roll_images[0], (250, 150))
 
-    def draw(self):
+        for dice_index in range(1, 8):
+            time.sleep(0.1)
+            rolling_dice = self.dice_roll_images[dice_index]
+            screen.blit(rolling_dice, (const.GAME_WIDTH / 2 - rolling_dice.get_width() / 2, const.GAME_HEIGHT / 2 - rolling_dice.get_height() / 2))
+            pygame.display.update()
+        
+        self.value = random.randint(1, 6)
+        dice_image = self.dice_images[self.value-1]
+        screen.blit(dice_image, (const.GAME_WIDTH / 2 - dice_image.get_width() / 2, const.GAME_HEIGHT / 2 - dice_image.get_height() / 2))
+
+        
+
+
+
+    #  is_rolling = True
+    #     rolling_aud.play()
+    #     rand_num = random.randint(0, 5)
+    #     dice_num_image = dice_images[rand_num]
+    #     screen.blit(dice_rolling_images[rolling_images_counter], (250, 150))
+    #     rolling_images_counter += 1
+    #     first = True
+
+    #     start rolling and calculate dice num
+    # else:
+    #     if is_rolling:
+    #         screen.blit(dice_rolling_images[rolling_images_counter], (250, 150))
+    #         rolling_images_counter += 1
+    #         if rolling_images_counter >= 8:
+    #             is_rolling = False
+    #             rolling_images_counter = 0
+
+    #     else:
+    #         screen.blit(dice_num_image, (250, 150))
+    #         if first:
+    #             rolling_stop_aud.play()
+    #             first = False
+
+
+    def determine_dice(self):
         if(self.is_rolling):
-            dice_image = self.dice_roll_images[self.value - 1]
-            screen.blit(dice_image, (const.GAME_WIDTH / 2 - dice_image.get_width() / 2, const.GAME_HEIGHT / 2 - dice_image.get_height() / 2))
+            #dice_image = 
+            return self.dice_roll_images[self.value - 1]
+            #screen.blit(dice_image, (const.GAME_WIDTH / 2 - dice_image.get_width() / 2, const.GAME_HEIGHT / 2 - dice_image.get_height() / 2))
         else:    
-            dice_image = self.dice_images[self.value - 1]
-            screen.blit(dice_image, (const.GAME_WIDTH / 2 - dice_image.get_width() / 2, const.GAME_HEIGHT / 2 - dice_image.get_height() / 2))
+            #dice_image = 
+            return self.dice_images[self.value - 1]
+            #screen.blit(dice_image, (const.GAME_WIDTH / 2 - dice_image.get_width() / 2, const.GAME_HEIGHT / 2 - dice_image.get_height() / 2))
--- maexchen.py
+++ maexchen.py
@@ -4,13 +4,19 @@ from dice import Dice
 import const
 
 pygame.init()
-screen = pygame.display.set_mode((const.GAME_WIDTH, const.GAME_HEIGHT))
-clock = pygame.time.Clock()
+pygame.display.set_caption("Mäxchen")
 
 class Maexchen():
     
     def __init__(self):
+        self.init_game_screen()
         self.dice = Dice()
+
+    def init_game_screen(self):
+        background_image = pygame.image.load("assets/background.png")
+        self.screen = pygame.display.set_mode((const.GAME_WIDTH, const.GAME_HEIGHT))
+        self.screen.blit(background_image, (0, 0))
+        self.clock = pygame.time.Clock()
 
     def handle_events(self):
         for event in pygame.event.get():
@@ -18,22 +24,26 @@ class Maexchen():
                 return False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    self.dice.roll()
+                    self.dice.roll2(self.screen)
         return True
 
     def update(self):
         pass
 
     def draw(self):
-        screen.fill(const.WHITE)
-        self.dice.draw()
-        pygame.display.flip()
+        self.blit_dice()
+        pygame.display.update()
+
+    def blit_dice(self):
+        dice_image = self.dice.determine_dice()
+        self.screen.blit(dice_image, (const.GAME_WIDTH / 2 - dice_image.get_width() / 2, const.GAME_HEIGHT / 2 - dice_image.get_height() / 2))
+
+
 
     def run(self):
         running = True
         while running:
-            clock.tick(const.FPS)
-
+            self.clock.tick(const.FPS)
             running = self.handle_events()
             self.update()
             self.draw()
