81e3a3375d8ce40d2b85c00784f409bcdf95ef9a
B9nsai <55996046+B9nsai@users.noreply.github.com>
2023-06-06 10:06:18 +0200

    Leon branch (#4)
    * inital commit leon-branch
    
    * generated code vom chatgpt
    
    * test
    
    * initial commit for maexchen - clean up of old files
    
    * implemented roll dice functionality
    
    * maexchen value logic implemented
    
    * made Dice an object, created different modules for each class, added main module maexchen.py
    
    * made maexchen an object
    
    * added statement in dice
    
    * removed statement in dice - should be implemented as another dice
    
    * implemented prompts in maexchen and pseudo maexchen loop - buggy btw
    
    * renamed dice to dice_logic, renamed maexchen to maexchen_logic

---
+++ maexchen_logic.py
@@ -0,0 +1,43 @@
+import os
+
+from logic import Logic
+
+class Maexchen():
+
+    def __init__(self):
+        self.__logic = Logic()
+
+    def play(self):
+        while not self.__prompt_play():
+            pass
+
+        while True:
+            self.__roll_dice()
+
+
+            self.__clear_console()
+            is_reveal = self.__prompt_believe()
+            
+            if is_reveal:
+                break
+                #__prompt_reveal()
+                #playagain?
+
+
+    def __roll_dice(self):
+        print("You rolled ", self.__logic.roll_dice())
+
+    def __prompt_play(self):
+        return input("Play Maexchen? [y/n]: ").__contains__('y')
+
+    def __prompt_believe(self):
+        return input("Believe and roll dice? [y/n]: ").__contains__('y')
+        
+
+
+    def __clear_console(self):
+        lambda: os.system("cls")
+        lambda: os.system("clear")
+
+maexchen = Maexchen()
+maexchen.play()
